9d481fecf0621b93c42bbc667d6b8c2c19cedae6
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-06-02 15:06:06 +0300

    Added translations of Pokemon names and descriptions

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,9 +5,12 @@ from django.utils import timezone
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='pokemons', blank=True)
+    description = models.TextField(blank=True, null=True)
+    title_en = models.CharField(max_length=200, blank=True)
+    title_jp = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
-        return format(self.title)
+        return '{}'.format(self.title, self.title_en, self.title_jp)
 
 
 class PokemonEntity(models.Model):
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -79,6 +79,9 @@ def show_pokemon(request, pokemon_id):
             'pokemon_id': requested_pokemon.id,
             'img_url': request.build_absolute_uri(requested_pokemon.image.url) if requested_pokemon.image else None,
             'title_ru': requested_pokemon.title,
+            'description': requested_pokemon.description,
+            'title_en': requested_pokemon.title_en,
+            'title_jp': requested_pokemon.title_jp
         })
 
     return render(request, 'pokemon.html', context={
